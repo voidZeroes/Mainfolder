9f7e57aa7bbdf14d12f404ea676c0f9a41d3ab50
voidZeroes <void.zeroes@gmail.com>
2020-05-13 14:46:11 +0900

    大規模な反映失敗を修正

---
+++ Shooting 2d/Assets/Scripts/BoomElace.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoomElace : MonoBehaviour
+{
+    public void DestroyBoom()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Shooting 2d/Assets/Scripts/Rate_of_Fire.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rate_of_Fire : MonoBehaviour
+{
+    int[] rateList = new int[100];
+    // Start is called before the first frame update
+    void Start()
+    {
+        rateList[0] = 1;//デフォ
+        rateList[0] = 10;//ノーマルショット
+        rateList[0] = 50;//味噌
+    }
+
+    // Update is called once per frame
+public int SetWeaponRate(int type)
+    {
+        return rateList[type];
+    }
+}
