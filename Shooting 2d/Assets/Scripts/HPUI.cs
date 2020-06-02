10c2d8bb3882e00d971141feb5191572fa5f1a77
voidZeroes <void.zeroes@gmail.com>
2020-06-02 15:40:05 +0900

    エネタンを並べる処理デケタ あとは透過させればできる

--- Shooting 2d/Assets/Scripts/HPUI.cs
+++ Shooting 2d/Assets/Scripts/HPUI.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HPUI : MonoBehaviour
 {
-   public GameObject energyTank;
+   public GameObject energyTank;//タンクプレハブ
+    public GameObject UI;//UI本体
+   public GameObject tankPos1;//設置位置
     public GameObject player;
     public Text text;
     float offsetX;
     float tankFloat;
+    int tankMaxNum;
     int tankNum;
+    int tankOldMaxNum;
     int playerLife;//総量
-    int sliderCnt;//0～99
+
 
     public Slider slider;
     // Start is called before the first frame update
@@ -21,8 +26,10 @@ public class HPUI : MonoBehaviour
         playerLife = player.GetComponent<MovePlayer>().SetPlayerLife();
         tankFloat = playerLife % 99;
         tankNum = playerLife/99;
+        tankMaxNum = player.GetComponent<MovePlayer>().SetPlayerMaxLife()/99;
         slider.value = 99;
-        
+        tankOldMaxNum = 0;
+        offsetX = 25;
     }
 
     // Update is called once per frame
@@ -31,37 +38,49 @@ public class HPUI : MonoBehaviour
         playerLife = GetHp();//毎回Player側のHPを取得
 
         tankFloat = playerLife % 99;//ゲージ内部の量
-        tankNum = playerLife/99;//タンクの量
+        tankNum = playerLife / 99;//タンクの量
 
         slider.value = tankFloat;
-        text.text =slider.value.ToString();//量をテキストに
+        text.text = slider.value.ToString();//量をテキストに
+
+        if (tankOldMaxNum < tankMaxNum)
+        { 
+        for(int i=0;i< tankMaxNum-tankOldMaxNum;i++)
+            {
+                var pos = tankPos1.transform.position;
+                pos.x += offsetX * i;
+                pos.z = -10;
+                Instantiate(energyTank, pos,Quaternion.identity,UI.transform);
+                tankOldMaxNum++;
+            }
+        }
 
 //        TankNumの量だけタンクを表示させたい
 //タンク1個目の座標 + Offset量 * TankNumでいいんだろうか
 //↑増加手順ならそれでOK
 //消去を考えたい
 
-//てきとうなObjectの下にぶらさげて、Childのナンバーで管理してみるか ?
+            //てきとうなObjectの下にぶらさげて、Childのナンバーで管理してみるか ?
 
-        //そうじゃん参考元だと取得済みのやつは透過させるだけじゃん。
-        //増えるやつだけ考えればOK
+            //そうじゃん参考元だと取得済みのやつは透過させるだけじゃん。
+            //増えるやつだけ考えればOK
 
 
 
-        //if(slider.value==-1)
-        //{
-        //    tankFloat = playerLife / 99;
-        //    tankNum =(int)tankFloat;
+            //if(slider.value==-1)
+            //{
+            //    tankFloat = playerLife / 99;
+            //    tankNum =(int)tankFloat;
 
-        //    slider.value = (int)(tankFloat-tankNum)*99;
-        //}
-        //else
-        //{
-        //    slider.value=
-        //}
-        //99以上の体力の時にゲージを1週させたい。
-        //99を割って、その数をストック。下限に達するたびにそれから１減らしてゲージを充填……で大丈夫だろうか。
-        //上限に達したらストックを１増やして、そのあと、99以上の部分をゲージに反映したらいい。
+            //    slider.value = (int)(tankFloat-tankNum)*99;
+            //}
+            //else
+            //{
+            //    slider.value=
+            //}
+            //99以上の体力の時にゲージを1週させたい。
+            //99を割って、その数をストック。下限に達するたびにそれから１減らしてゲージを充填……で大丈夫だろうか。
+            //上限に達したらストックを１増やして、そのあと、99以上の部分をゲージに反映したらいい。
 
 
 
